e2b23b53b391cc0896797d8c49a4e0fe21f8e6b7
Francis lalaguna <franc@UBVPNN7599.core.unionbank.com>
2024-04-19 10:41:24 +0800

    update image fetch

--- src/components/OfficialMembers.js
+++ src/components/OfficialMembers.js
@@ -1,14 +1,15 @@
 import '../styles/Cards.css'
-const OfficialMember = ({ image, date, title, info, location, duration, cost }) => {
+// const OfficialMember = ({ image, date, title, info, location, duration, cost }) => {
+  const OfficialMember = ({ filename, name, info, title}) => {
   return (
     <article className='tour-card'>
       <div className='tour-img-container'>
-        <img src={image} className='tour-img' alt={title} />
-        <p className='tour-date'>{date}</p>
+        <img src={`http://ec2-13-231-167-201.ap-northeast-1.compute.amazonaws.com:3000/images/`+ filename} className='tour-img' alt={title} />
+        <p className='tour-date'>{title}</p>
       </div>
       <div className='tour-info'>
         <div className='tour-title'>
-          <h4>{title}</h4>
+          <h4>{name}</h4>
         </div>
         <p>{info}</p>
         <div className='tour-footer'>
@@ -16,7 +17,7 @@ const OfficialMember = ({ image, date, title, info, location, duration, cost }) 
             <span>
               <i className='fas fa-map'></i>
             </span>
-            {location}
+            {title}
           </p>
         </div>
       </div>
